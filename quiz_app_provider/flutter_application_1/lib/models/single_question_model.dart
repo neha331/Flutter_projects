e33b1e77f60276e0734bb04d1bcde01c70075ade
neha shedage <nehashedage99@gmail.com>
2024-05-21 18:17:04 +0530

    refracter code from setstate to provider

---
+++ quiz_app_provider/flutter_application_1/lib/models/single_question_model.dart
@@ -0,0 +1,7 @@
+class SingleQuestionMode {
+ final  String? question;
+ final  List<String>? options;
+ final  int?  answerIndex; 
+
+ const SingleQuestionMode({this.question, this.options, this.answerIndex});
+}
